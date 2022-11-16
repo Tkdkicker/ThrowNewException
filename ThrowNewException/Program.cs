6a1a2228c37e67371261cce8060d1ad1bcc8e839
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 10:53:23 +0000

    Add project files.

---
+++ ThrowNewException/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ThrowNewException
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DisplayFirstNumber(args);
+        }
+
+        private static void DisplayFirstNumber(string[] args)
+        {
+            string arg = args.Length >= 1 ? args[0] :
+                                       throw new ArgumentException("You must supply an argument");
+            if (long.TryParse(arg, out var number))
+            {
+                Console.WriteLine($"You entered {number:F0}");
+            }
+            else
+            {
+                Console.WriteLine($"{arg} is not a number.");
+            }
+        }
+    }
+}
